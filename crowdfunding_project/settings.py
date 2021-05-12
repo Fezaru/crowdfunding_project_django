99dd568a357411f91495f283eff29752f93a6121
Fezaru <nekhaichik.a@gmail.com>
2021-05-12 19:11:41 +0300

    hidden the secret key

--- crowdfunding_project/settings.py
+++ crowdfunding_project/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
 from pathlib import Path
+from crowdfunding_project.secret_key import secret_key
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-=gf)oego+z5&ny5_ktb8k!&suzxbu3%!3*)wncc0h__a-cqr3n'
+SECRET_KEY = secret_key
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
