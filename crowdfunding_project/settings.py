b776266eed18c8f125b80363e000a8aae0ca2f48
Fezaru <nekhaichik.a@gmail.com>
2021-06-11 21:44:48 +0300

    added localhost to allowed hosts

--- crowdfunding_project/settings.py
+++ crowdfunding_project/settings.py
@@ -21,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-# SECRET_KEY = secret_key
+# SECRET_KEY = secret_key  # поменять пушо файл удален
 SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
@@ -29,6 +29,7 @@ DEBUG = os.environ.get('DEBUG_VALUE')
 
 ALLOWED_HOSTS = [
     'django-crowdfunding-project.herokuapp.com',
+    'localhost',
 ]
 
 
